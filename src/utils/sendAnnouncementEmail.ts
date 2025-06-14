f069f4e07e2164173839c53ce0dc4ff521702ff2
ushasri2645 <ugudikandula@everest.engineering>
2025-06-14 14:44:56 +0530

    Change the logic to handle include, exclude and all case properly.

--- src/utils/sendAnnouncementEmail.ts
+++ src/utils/sendAnnouncementEmail.ts
@@ -5,6 +5,7 @@ import { Announcement } from "../models/Announcement";
 import { SubscriberInstance } from "../types/subscriber.types";
 
 import dotenv from "dotenv";
+import { getEmail } from "./getAdminEmail";
 
 dotenv.config();
 
@@ -13,60 +14,76 @@ const EMAIL_PASS = process.env.EMAIL_PASS as string;
 
 interface EmailTargets {
   mode: "all" | "include" | "exclude";
-  users?: number[];
+  users?: string[];
 }
 
 export default async function sendAnnouncementEmails(
   announcement: Announcement
 ): Promise<void> {
-  console.log("sendinf")
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: EMAIL_USER,
-      pass: EMAIL_PASS,
-    },
-  });
-
   const allSubscribers: SubscriberInstance[] = (
     await Subscriber.findAll({ where: { is_active: true } })
   ).map((sub) => sub.get() as SubscriberInstance);
   const emailTargets: EmailTargets = {
-    mode:  announcement.email_targets || "all" ,
+    mode: announcement.email_targets.mode || "all",
+    users:
+      announcement.email_targets.mode !== "all"
+        ? announcement.email_targets.users
+        : allSubscribers,
   };
   let recipients: SubscriberInstance[] = [];
-  console.log(allSubscribers)
-
+  console.log("et", emailTargets);
   switch (emailTargets.mode) {
     case "all":
       recipients = allSubscribers;
       break;
     case "include":
       recipients = allSubscribers.filter((sub) =>
-        emailTargets.users?.includes(sub.id)
+        emailTargets.users?.includes(sub.email)
       );
       break;
     case "exclude":
       recipients = allSubscribers.filter(
-        (sub) => !emailTargets.users?.includes(sub.id)
+        (sub) => !emailTargets.users?.includes(sub.email)
       );
       break;
   }
 
-  const html = generateAnnouncementEmail(announcement);
+  console.log("r", recipients);
+  
 
-  console.log("html", recipients)
+  const result = await getEmail();
+
+  if (!result) {
+    console.log("no result");
+    // res.status(503).json({ message: "Service unavailable" });
+    return;
+  }
+
+  const { sendingEmail, password } = result;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: sendingEmail,
+      pass: password,
+    },
+  });
 
   for (const sub of recipients) {
+    console.log("came to send");
+    const html = generateAnnouncementEmail(announcement,sub.name || 'there');
     await transporter.sendMail({
       from: `"Your App" <${EMAIL_USER}>`,
-      to: 'ushasrigudikandula456@gmail.com',
+      to: sub.email,
       subject: announcement.title,
       html,
     });
-    console.log("email senr")
   }
+
 
   announcement.is_email_sent = true;
   await announcement.save();
+  console.log("Sent succesfully")
 }
