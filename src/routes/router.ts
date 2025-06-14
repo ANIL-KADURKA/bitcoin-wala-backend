f69d79df145e7fdc1b2525be0a810780981a5f77
ushasri2645 <ugudikandula@everest.engineering>
2025-06-14 14:45:54 +0530

    Add a global router.

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -3,9 +3,13 @@ import { adminRouter } from "./admin-registration";
 import { bitcoinRouter } from "./bitcoin";
 import { subscriptionRoouter } from "./subscribe";
 import { announcementRouter } from "./announcement";
+import '../cron/announcementCron'
+import { cronRouter } from "./cronRouter";
+
 export const router = express.Router();
 
 router.use("/admin", adminRouter);
 router.use("/bitcoin", bitcoinRouter);
 router.use("/subscription", subscriptionRoouter);
 router.use("/announcement", announcementRouter);
+router.use("/cron", cronRouter);
