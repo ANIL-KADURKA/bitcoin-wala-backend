212a9ca150250a677a2d8703f52dc1932f0072ab
ushasri2645 <ugudikandula@everest.engineering>
2025-06-16 08:50:02 +0530

    Add cors.

--- app.ts
+++ app.ts
@@ -10,7 +10,13 @@ import { router } from "./src/routes/router";
 const app = express();
 const port: number = parseInt(process.env.PORT || "5000");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use("/", router);
 app.get("/", async (req, res) => {
